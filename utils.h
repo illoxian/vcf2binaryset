cb096f14290c8548925bc05af44921c7a6698987
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2020-09-24 17:55:01 +0200

    Domande

--- utils.h
+++ utils.h
@@ -1,13 +1,14 @@
 #pragma once
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <vector>
 
 /* Splits input string at each occurence of char delim. */
-std::vector<std::string> split_string(const std::string& input, char delim, unsigned int expectedSize = 1);
+std::vector<std::string> split_string(const std::string& input, char delim,
+                                      unsigned int expectedSize = 1);
 
 /* Checks whether arguments contain given option. */
-bool has_cmd_option(char **begin, char **end, std::string option);
+bool has_cmd_option(char** begin, char** end, std::string option);
 
 /* Returns argument for option if it exists. */
-char* get_cmd_option(char **begin, char **end, std::string option);
+char* get_cmd_option(char** begin, char** end, std::string option);
